1dc42208231300261a3b7d994ad88f4f3e184bb9
Igor Pavlov <gophan1992@gmail.com>
2016-08-27 21:11:00 +0300

    fixed error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def find_meta_tags
     found_meta_tags = MetaTag.where(url: request.path)
     if found_meta_tags.present?
-      meta_tag = meta_tags.take
+      meta_tag = found_meta_tags.take
       set_meta_tags title:        meta_tag.title,
                     description:  meta_tag.description,
                     keywords:     meta_tag.keywords
